ffc9c49fc27eabf37a09ecb303df983a4f64bf54
Jordan <jordan.l.partridge@gmail.com>
2024-09-30 20:03:49 -0700

    project added

---
+++ app/Events/ProjectCreated.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Project;
+use App\States\ProjectState;
+use Thunk\Verbs\Attributes\Autodiscovery\StateId;
+use Thunk\Verbs\Event;
+
+class ProjectCreated extends Event
+{
+    #[StateId(ProjectState::class)]
+    public ?int $project_id = null;
+
+    public string $name;
+
+    public ?string $description = null;
+
+    public function applyToProject(ProjectState $project): void
+    {
+        $project->name = $this->name;
+        $project->description = $this->description;
+    }
+
+    public function handle()
+    {
+        return Project::create([
+            'name' => $this->name,
+            'description' => $this->description,
+        ]);
+    }
+}
---
+++ app/Models/Project.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Glhd\Bits\Database\HasSnowflakes;
+use Glhd\Bits\Snowflake;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
+
+class Project extends Model
+{
+    use HasFactory;
+    use HasSlug;
+    use HasSnowflakes;
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    protected $casts = [
+        'id' => Snowflake::class,
+    ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+}
