266e4fa2796d9131c269cc178389c8a315e2c6a7
Jordan <jordan.l.partridge@gmail.com>
2024-11-25 19:34:57 -0700

    better commit replationship manager

--- app/Filament/Resources/RepoResource/RelationManagers/CommitsRelationshipManager.php
+++ app/Filament/Resources/RepoResource/RelationManagers/CommitsRelationshipManager.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\RepoResource\RelationManagers;
 
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
@@ -12,7 +13,11 @@ use Filament\Tables\Actions\DeleteAction;
 use Filament\Tables\Actions\DeleteBulkAction;
 use Filament\Tables\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 
 class CommitsRelationshipManager extends RelationManager
@@ -69,9 +74,56 @@ class CommitsRelationshipManager extends RelationManager
                     ->searchable()
                     ->sortable()
                     ->toggleable(),
+
+                TextColumn::make('committed_at')
+                    ->label('Committed at:')
+                    ->dateTime()
+                    ->formatStateUsing(fn ($state) => $state->format('M j, Y g:i A'))
+                    ->sortable()
+                    ->toggleable(),
             ])
             ->filters([
-                //
+                SelectFilter::make('author')
+                    ->options(function () {
+                        return $this->getOwnerRecord()
+                            ->commits()
+                            ->distinct()
+                            ->pluck('author', 'author')
+                            ->toArray();
+                    })
+                    ->label('Filter by Author')
+                    ->multiple()
+                    ->preload(),
+                Filter::make('committed_at')
+                    ->form([
+                        DatePicker::make('from'),
+                        DatePicker::make('to'),
+                    ])
+                    ->query(function (Builder $query, array $data): Builder {
+                        return $query
+                            ->when(
+                                $data['from'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('committed_at', '>=', $date),
+                            )
+                            ->when(
+                                $data['to'],
+                                fn (Builder $query, $date): Builder => $query->whereDate('committed_at', '<=', $date),
+                            );
+                    })
+                    ->indicateUsing(function (array $data): array {
+                        $indicators = [];
+
+                        if ($data['from'] ?? null) {
+                            $indicators['from'] = 'From ' . Carbon::parse($data['from'])->toFormattedDateString();
+                        }
+
+                        if ($data['to'] ?? null) {
+                            $indicators['to'] = 'To ' . Carbon::parse($data['to'])->toFormattedDateString();
+                        }
+
+                        return $indicators;
+                    }),
+
             ])
             ->headerActions([
                 CreateAction::make(),
