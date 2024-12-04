f621f74a215b8aab79661e5d8c4cca9ce113cbb7
Jordan Partridge <jordan.l.partridge@gmail.com>
2024-12-04 15:06:11 -0700

    access controll

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -27,10 +27,9 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
      */
     protected function gate(): void
     {
+
         Gate::define('viewHorizon', function ($user) {
-            return in_array($user->email, [
-                //
-            ]);
+            return $user->email === 'jordan@partridge.rocks';
         });
     }
 }
