e2a8a38ce8a0182521654ff001f8b6efdfca9073
Jordan Partridge <jordan.l.partridge@gmail.com>
2024-12-07 21:19:33 -0700

    owners set to soft deletes for consistency

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -5,11 +5,13 @@ namespace App\Models;
 use Database\Factories\OwnerFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Owner extends Model
 {
     /** @use HasFactory<OwnerFactory> */
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'login',
