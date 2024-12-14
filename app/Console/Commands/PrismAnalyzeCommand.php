59ce8aec0834a5561cf808dbf1394839b0e3e155
Jordan Partridge <jordan.l.partridge@gmail.com>
2024-12-13 17:01:59 -0700

    feat: Add AI-powered code analysis command using Prism

---
+++ app/Console/Commands/PrismAnalyzeCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class PrismAnalyzeCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:prism-analyze-command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
